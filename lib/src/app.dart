d17084f75a41ec3bd93f0d48c1df605004bd70b3
mhmdgazzar <mhmdgazzar@gmail.com>
2024-06-18 20:36:18 +0200

    ip API

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:onepage_api/src/features/mainscreen.dart';
+import 'package:onepage_api/src/features/mainscreen/presentation/mainscreen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Mainscreen(),
+    return MaterialApp(
+      home: const Mainscreen(),
+      theme: ThemeData.from(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.black)),
     );
   }
 }
---
+++ lib/src/features/mainscreen/presentation/mainscreen.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+class Mainscreen extends StatefulWidget {
+  const Mainscreen({super.key});
+
+  @override
+  State<Mainscreen> createState() => _MainscreenState();
+}
+
+class _MainscreenState extends State<Mainscreen> {
+  String ip = 'Your IP Address';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(ip),
+            const SizedBox(height: 16),
+            OutlinedButton(
+              onPressed: () async {
+                final response =
+                    await get(Uri.parse('api.ipify.org/?format=json'));
+
+                Map<String, dynamic> m = jsonDecode(response.body);
+
+                setState(() {
+                  ip = m['ip'];
+                });
+              },
+              child: const Text('Refresh API'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
